e301495c8841ba1c178dcc8f080ad6f3537a8fd2
Matthew Kennedy <burnsidemk@gmail.com>
2020-08-14 12:49:22 -0500

    Appveyor

--- ci-build.cmd
+++ ci-build.cmd
@@ -1,3 +1,5 @@
-set CHERE_INVOKING yes
-set MSYSTEM MINGW64
+rem C:\msys64\user\bin\bash -lc "pacman -Syuu"  # Core update (in case any core packages are outdated)
+rem C:\msys64\user\bin\bash -lc "pacman -Syuu"  # Normal update
+set CHERE_INVOKING=yes
+set MSYSTEM=MINGW64
 C:\msys64\user\bin\bash -lc "./ci-build.sh"
