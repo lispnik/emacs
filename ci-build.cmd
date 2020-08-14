f67109439e697c0ef5e5d7d80689ea077e210ab6
Matthew Kennedy <burnsidemk@gmail.com>
2020-08-14 13:08:56 -0500

    Appveyor

--- ci-build.cmd
+++ ci-build.cmd
@@ -1,6 +1,5 @@
-rem C:\msys64\user\bin\bash -lc "pacman -Syuu"  # Core update (in case any core packages are outdated)
-rem C:\msys64\user\bin\bash -lc "pacman -Syuu"  # Normal update
-C:\msys64\usr\bin\bash -lc "pacman -Syu mingw64/mingw-w64-x86_64-gnutls"
+C:\msys64\usr\bin\bash -lc "pacman -Syuu"
+C:\msys64\usr\bin\bash -lc "pacman -Syuu"
 set CHERE_INVOKING=yes
 set MSYSTEM=MINGW64
 C:\msys64\usr\bin\bash -lc "./ci-build.sh"
