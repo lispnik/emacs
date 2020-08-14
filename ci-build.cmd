4edd9e16221f0247d4449a741afaccf72c765b91
Matthew Kennedy <burnsidemk@gmail.com>
2020-08-14 13:02:46 -0500

    Appveyor

--- ci-build.cmd
+++ ci-build.cmd
@@ -2,12 +2,4 @@ rem C:\msys64\user\bin\bash -lc "pacman -Syuu"  # Core update (in case any core 
 rem C:\msys64\user\bin\bash -lc "pacman -Syuu"  # Normal update
 set CHERE_INVOKING=yes
 set MSYSTEM=MINGW64
-dir c:\
-dir c:\msys64
-dir c:\projects\
-dir c:\projects\emacs
-dir c:\msys64\user\bin\bash
-cd
-dir ci-build.sh
-dir c:\projects\emacs
-C:\msys64\user\bin\bash -lc "./ci-build.sh"
+C:\msys64\usr\bin\bash -lc "./ci-build.sh"
