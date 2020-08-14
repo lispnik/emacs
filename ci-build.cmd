567d44b1a90a90b56af7f52a28fd5e1cdcfb81fd
Matthew Kennedy <burnsidemk@gmail.com>
2020-08-14 13:00:54 -0500

    Appveyor

--- ci-build.cmd
+++ ci-build.cmd
@@ -4,4 +4,10 @@ set CHERE_INVOKING=yes
 set MSYSTEM=MINGW64
 dir c:\
 dir c:\msys64
+dir c:\projects\
+dir c:\projects\emacs
+dir c:\msys64\user\bin\bash
+cd
+dir ci-build.sh
+dir c:\projects\emacs
 C:\msys64\user\bin\bash -lc "./ci-build.sh"
